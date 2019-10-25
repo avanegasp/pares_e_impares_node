a27e7bd48d955f24cf5eaf6ed7385aee5502d7a3
Angie <arco527@gmail.com>
2019-10-24 22:45:49 -0500

    Result with pug

--- app.js
+++ app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  let result = "";
-  for (var i = 1; i <= 50; i++) {
-    if (i % 2 == 0) {
-      result += "<p>" + i + " Soy Par!" + "</p>"
-    }else{
-      result += "<p>" + i + " Soy Impar!" + "</p>"
-    }
-  }
-  res.send(result)
-})
+
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
+
+app.get('/hello', (req, res) => {
+  res.render('index.pug', { mensaje: 'Usando Pug JS en Express' }); // Se muestra la plantilla hello.pug
+});
 
 app.listen(3000, () => console.log('Listening on port 3000'));
+
+// app.get('/', (req, res) => {
+//   let result = "";
+//   for (var i = 1; i <= 50; i++) {
+//     if (i % 2 == 0) {
+//       result += "<p>" + i + " Soy Par!" + "</p>"
+//     }else{
+//       result += "<p>" + i + " Soy Impar!" + "</p>"
+//     }
+//   }
+//   res.send(result)
+// })
